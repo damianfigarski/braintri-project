584a56fd4c8596a2e1eca8c07a7d9ebbcd3652bb
Damian Figarski <damian.figarski@doddlecode.com>
2018-06-07 21:31:23 +0200

    Cross origin added

--- src/main/java/com/doddlecode/braintriproject/BraintriProjectApplication.java
+++ src/main/java/com/doddlecode/braintriproject/BraintriProjectApplication.java
@@ -2,12 +2,14 @@ package com.doddlecode.braintriproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:4200")
 public class BraintriProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BraintriProjectApplication.class, args);
     }
-	
+
 }
